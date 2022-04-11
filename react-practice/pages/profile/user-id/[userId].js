0cbf6c76a7ec5eb589ad061bea609c201f02a103
kwaksj329 <kwaksj329@naver.com>
2022-04-12 03:32:51 +0900

    client side rendering code

--- react-practice/pages/profile/user-id/[userId].js
+++ react-practice/pages/profile/user-id/[userId].js
@@ -1,28 +1,38 @@
-import {getUserProfileByUserId} from "../../../model/profile/profile.model";
+// import {getUserProfileByUserId} from "../../../model/profile/profile.model";
+import useSWR from "swr";
+import {useRouter} from "next/router";
 
 const ProfilePage = ({
-    username,
-    major,
-    age
+    // username,
+    // major,
+    // age
     
 }) => {
+    const router = useRouter();
+    const {userId} = router.query;
+    const {data, error} = useSWR(`/api/profile?userId=${userId}`)
+
+    if (error) {return <div>failed to load</div>;}
+    if (!data) {return <div>loading...</div>;}
+
     return (
         <div>
-            <p>username : {username}</p>
-            <p>major : {major}</p>
-            <p>age : {age}</p>
+            <h1>Profile</h1>
+            <p>username : {data.username}</p>
+            <p>major : {data.major}</p>
+            <p>age : {data.age}</p>
         </div>
     );
 };
 //SEO = Search Engine Optimization
 
-export async function getServerSideProps(context){
-    const {userId} = context.query.userId;
-    const userProfile = getUserProfileByUserId(userId);
+// export async function getServerSideProps(context){
+//     const {userId} = context.query.userId;
+//     const userProfile = getUserProfileByUserId(userId);
 
-    return{
-        props: userProfile
-    };
-}
+//     return{
+//         props: userProfile
+//     };
+// }
 
 export default ProfilePage;
