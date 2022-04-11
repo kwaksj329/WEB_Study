e5a7134dfe1b229c7235787ee92b02b6f1a79446
kwaksj329 <kwaksj329@naver.com>
2022-04-12 03:19:13 +0900

    server side rendering code

--- react-practice/model/profile/profile.model.js
+++ react-practice/model/profile/profile.model.js
@@ -1,9 +1,17 @@
 export function getUserProfileByUserId(userId) {
-    return {
-        username: "Ikanny",
-        major: "Computer engineering",
-        age: 20
-    };
+    if(parseInt(userId) == 1){
+        return {
+            username: "Ikanny",
+            major: "Computer engineering",
+            age: 20
+        };
+    }else{
+        return {
+            username: "Sujong",
+            major: "Computer engineering",
+            age: 23
+        };
+    }
 }
 
 export function putUserProfile(userProfile) {
--- react-practice/pages/profile/user-id/[userId].js
+++ react-practice/pages/profile/user-id/[userId].js
@@ -1,9 +1,28 @@
-const ProfilePage = () => {
+import {getUserProfileByUserId} from "../../../model/profile/profile.model";
+
+const ProfilePage = ({
+    username,
+    major,
+    age
+    
+}) => {
     return (
         <div>
-            ProfilePage
+            <p>username : {username}</p>
+            <p>major : {major}</p>
+            <p>age : {age}</p>
         </div>
     );
 };
+//SEO = Search Engine Optimization
+
+export async function getServerSideProps(context){
+    const {userId} = context.query.userId;
+    const userProfile = getUserProfileByUserId(userId);
+
+    return{
+        props: userProfile
+    };
+}
 
 export default ProfilePage;
